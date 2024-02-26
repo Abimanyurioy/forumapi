c8d7aa1eb0fd5b3769a008482adf16e69784a9be
abimanyu.rio <abimanyu.rio@imfi.com>
2024-02-26 09:11:33 +0700

    add error test case

--- src/Infrastructures/http/_test/comment_likes.test.js
+++ src/Infrastructures/http/_test/comment_likes.test.js
@@ -163,4 +163,70 @@ describe("/threads/{threadId}/comments/{commentId}/likes endpoint", () => {
       expect(responseJson.status).toEqual("success");
     });
   });
+
+  describe("when remove /likes", () => {
+    it("should response 200 with status success", async () => {
+      // Arrange
+      const server = await createServer(container);
+
+      // Add account
+      await server.inject({
+        method: "POST",
+        url: "/users",
+        payload: userPayload,
+      });
+      // login
+      const auth = await server.inject({
+        method: "POST",
+        url: "/authentications",
+        payload: loginPayload,
+      });
+      const authToken = JSON.parse(auth.payload)?.data?.accessToken;
+
+      // add thread
+      const thread = await server.inject({
+        method: "POST",
+        url: "/threads",
+        payload: threadPayload,
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      const threadId = JSON.parse(thread.payload)?.data?.addedThread.id;
+
+      // add comment
+      const comment = await server.inject({
+        method: "POST",
+        url: `/threads/${threadId}/comments`,
+        payload: commentPayload,
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      const commentId = JSON.parse(comment.payload)?.data?.addedComment.id;
+
+      // add comment likes
+      await server.inject({
+        method: "PUT",
+        url: `/threads/${threadId}/comments/${commentId}/likes`,
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      // Action remove comment likes
+      const response = await server.inject({
+        method: "POST",
+        url: `/threads/${threadId}/comments/${commentId}/likes`,
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      //Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.status).toEqual("success");
+    });
+  });
 });
